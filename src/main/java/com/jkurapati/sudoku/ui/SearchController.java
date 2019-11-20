91538d7195282a80b7ff88a7a61005e962663430
Jyothrilinga Kurapati <jyothri@jkurapati.com>
2019-11-19 23:08:48 -0800

    Update layout for sudoku.

--- src/main/java/com/jkurapati/sudoku/ui/SearchController.java
+++ src/main/java/com/jkurapati/sudoku/ui/SearchController.java
@@ -3,17 +3,47 @@ package com.jkurapati.sudoku.ui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 public class SearchController {
+    private static final int SUDOKU_DIMENSION = 3;
+
     @FXML
-    private TextField searchField;
+    private Button solveButton;
+
     @FXML
-    private Button searchButton;
+    private GridPane outerGridPane;
 
     @FXML
     private void initialize() {
-        // search panel
-        searchButton.setText("Search");
-//        searchButton.setOnAction(event -> loadData());
+        populateSudokuCells();
+
+        solveButton.setOnAction(event -> System.out.println("Got event:" + event));
+    }
+
+    private void populateSudokuCells() {
+        outerGridPane.getStyleClass().add("outerGridPane");
+        for (int i = 0; i < SUDOKU_DIMENSION; i++) {
+            for (int j = 0; j < SUDOKU_DIMENSION; j++) {
+                GridPane innerGridPane = new GridPane();
+                innerGridPane.getStyleClass().add("innerGridPane");
+                int block = i * SUDOKU_DIMENSION + j;
+                outerGridPane.add(innerGridPane, j, i, 1, 1);
+                populateBlock(block, innerGridPane);
+            }
+        }
+    }
+
+    private void populateBlock(int block, GridPane gridPane) {
+        for (int i = 0; i < SUDOKU_DIMENSION; i++) {
+            for (int j = 0; j < SUDOKU_DIMENSION; j++) {
+                TextField textField = new TextField();
+                String cellId = String.format("block%d-row%d-col%d", block, i, j);
+                textField.setId(cellId);
+                textField.setText("X");
+                textField.getStyleClass().add("cell");
+                gridPane.add(textField, j, i, 1, 1);
+            }
+        }
     }
 }
--- src/main/java/com/jkurapati/sudoku/ui/SudokuUI.java
+++ src/main/java/com/jkurapati/sudoku/ui/SudokuUI.java
@@ -1,14 +1,17 @@
 package com.jkurapati.sudoku.ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
 
 import java.io.IOException;
 
 public class SudokuUI extends Application {
+
+    private static final String FXML_FILE = "/com/jkurapati/sudoku/ui/SudokuController.fxml";
+    private static final String CSS_FILE = "/com/jkurapati/sudoku/ui/sudoku.css";
 
     public static void main(String[] args) {
         launch(args);
@@ -17,13 +20,13 @@ public class SudokuUI extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader(
-                SudokuUI.class.getResource("/com/jkurapati/sudoku/ui/SearchController.fxml"));
-        AnchorPane page = (AnchorPane) loader.load();
+                SudokuUI.class.getResource(FXML_FILE));
+        AnchorPane page = loader.load();
         Scene scene = new Scene(page);
 
-        scene.getStylesheets().add("/com/jkurapati/sudoku/ui/search.css");
+        scene.getStylesheets().add(CSS_FILE);
 
-        primaryStage.setTitle("Title goes here");
+        primaryStage.setTitle("Sudoku Solver");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
